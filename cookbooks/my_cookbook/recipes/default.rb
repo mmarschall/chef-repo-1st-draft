d699146b174e89172f9e956bdf257950a8c2fa2b
Matthias Marschall <mm@agileweboperations.com>
2013-03-08 16:13:20 +0100

    editing config files using augeas

--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -6,10 +6,26 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-template "/etc/logrotate.conf" do
-  source "logrotate.conf.erb"
-  variables(
-    :how_often => "daily",
-    :keep => "31"
-  )
+p = bash 'install augeas' do
+  code <<-EOH
+    apt-get update
+    apt-get -y install pkg-config ruby1.9.1-dev libaugeas-dev
+    wget http://download.augeas.net/augeas-1.0.0.tar.gz
+    tar xzf augeas-1.0.0.tar.gz
+    cd augeas-1.0.0
+    ./configure --prefix /opt/chef/embedded
+    make
+    make install
+  EOH
+  not_if "test -f /opt/chef/embedded/bin/augtool"
+end
+p.run_action(:run)
+
+chef_gem "ruby-augeas"
+
+require 'augeas'
+
+host = Augeas::open do |aug| 
+  aug.set("/files/etc/ssh/sshd_config/AcceptEnv[last()]/01", "FOO")
+  aug.save!
 end
