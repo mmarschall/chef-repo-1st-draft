133f0fe2398d5e043e48f78abc34f6c0da6d35db
Matthias Marschall <mm@agileweboperations.com>
2013-03-19 22:27:06 +0100

    installing from source using remote_file

--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -7,15 +7,14 @@
 # All rights reserved - Do Not Redistribute
 #
 version = "1.3.9"
-#remote_file "fetch_nginx_source" do
-#  source "http://nginx.org/download/nginx-#{version}.tar.gz"
-#  path "#{Chef::Config['file_cache_path']}/nginx-#{version}.tar.gz"
-#end
+remote_file "fetch_nginx_source" do
+  source "http://nginx.org/download/nginx-#{version}.tar.gz"
+  path "#{Chef::Config['file_cache_path']}/nginx-#{version}.tar.gz"
+end
 
 bash "install_nginx_from_source" do
   cwd Chef::Config['file_cache_path']
   code <<-EOH
-    wget http://nginx.org/download/nginx-#{version}.tar.gz
     tar zxf nginx-#{version}.tar.gz &&
     cd nginx-#{version} &&
     ./configure --without-http_rewrite_module &&
