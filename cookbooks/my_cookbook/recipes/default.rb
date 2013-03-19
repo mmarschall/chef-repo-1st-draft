f42b4615f680f7b2d9c83c671d0edc489b60380d
Matthias Marschall <mm@agileweboperations.com>
2013-03-19 22:26:43 +0100

    installing from source using wget

--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -6,13 +6,20 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-include_recipe "apt"
+version = "1.3.9"
+#remote_file "fetch_nginx_source" do
+#  source "http://nginx.org/download/nginx-#{version}.tar.gz"
+#  path "#{Chef::Config['file_cache_path']}/nginx-#{version}.tar.gz"
+#end
 
-apt_repository "s3tools" do
-  uri "http://s3tools.org/repo/deb-all"
-  components ["stable/"]
-  key "http://s3tools.org/repo/deb-all/stable/s3tools.key"
-  action :add
+bash "install_nginx_from_source" do
+  cwd Chef::Config['file_cache_path']
+  code <<-EOH
+    wget http://nginx.org/download/nginx-#{version}.tar.gz
+    tar zxf nginx-#{version}.tar.gz &&
+    cd nginx-#{version} &&
+    ./configure --without-http_rewrite_module &&
+    make && make install
+  EOH
+  not_if "test -f /usr/local/nginx/sbin/nginx"
 end
-
-package "s3cmd"
