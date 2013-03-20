d598b1d42140d0d240dd29f6771ca909746e81c3
Matthias Marschall <mm@agileweboperations.com>
2013-03-20 21:46:18 +0100

    triggering bash scripts if files change

--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -6,19 +6,13 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-version = "1.3.9"
-remote_file "fetch_nginx_source" do
-  source "http://nginx.org/download/nginx-#{version}.tar.gz"
-  path "#{Chef::Config['file_cache_path']}/nginx-#{version}.tar.gz"
+template "/tmp/trigger" do
+  notifies :run, "bash[run_on_trigger]", :immediately
 end
 
-bash "install_nginx_from_source" do
-  cwd Chef::Config['file_cache_path']
-  code <<-EOH
-    tar zxf nginx-#{version}.tar.gz &&
-    cd nginx-#{version} &&
-    ./configure --without-http_rewrite_module &&
-    make && make install
-  EOH
-  not_if "test -f /usr/local/nginx/sbin/nginx"
+bash "run_on_trigger" do
+  user "root"
+  cwd "/tmp"
+  code "echo 'Triggered'"
+  action :nothing
 end
