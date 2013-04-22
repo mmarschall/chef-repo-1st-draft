57230a55e7663883ffff5389c1ee3cec47bd4dd7
Matthias Marschall <mm@agileweboperations.com>
2013-04-22 22:19:37 +0200

    managing nginx sites

--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -6,7 +6,26 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-sudo 'mma' do
-  template    'staff_member.erb' # local cookbook template
-  variables   :cmds => ['/etc/init.d/ssh restart']
+include_recipe "nginx::source"
+
+app_name = "my_app"
+app_home = "/srv/#{app_name}"
+
+template "#{node[:nginx][:dir]}/sites-available/#{app_name}" do
+  source "nginx-site-#{app_name}.erb"
+  owner  "root"
+  group  "root"
+  mode   "0644"
+  variables :app_home => app_home
+  notifies :restart, resources(:service => "nginx")
 end
+
+directory "#{app_home}/public" do
+  recursive true
+end
+
+file "#{app_home}/public/index.html" do
+  content "<h1>Hello World!</h1>"
+end
+
+nginx_site "#{app_name}"  
--- roles/web_server.rb
+++ roles/web_server.rb
@@ -6,14 +6,9 @@ name "web_server"
 # and, if there are merge conflicts during site install:
 # git branch -D chef-vendor-apt (to delete the old vendor branch)
 # apt is there because of: http://stackoverflow.com/questions/12555096/error-with-chef-build-essential-cookbook-on-ubuntu-12-04
-run_list "recipe[nginx::source]"
+run_list "recipe[my_cookbook]"
 
 default_attributes "nginx" => { 
   "init_style" => "init",
-  "upload_progress" => {
-    "url" => "https://github.com/masterzen/nginx-upload-progress-module/tarball/v0.9.0"
-  },
-  "source" => {
-    "modules" => ["upload_progress_module"]
-  }
+  "default_site_enabled" => false
 }
