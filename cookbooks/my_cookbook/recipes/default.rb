70c321865a473faa3da96b9cdc701a75e493d697
Matthias Marschall <mm@agileweboperations.com>
2013-04-23 21:42:06 +0200

    mysql management

--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -7,10 +7,13 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# required cookbooks: 
+# required cookbooks: database (should install all others as well...)
 # build-essential, openssl, mysql, database, postgresql, xfs, aws
 
+# knife cookbook upload --all
+
 include_recipe 'mysql::server'
+include_recipe 'mysql::ruby'
 
 include_recipe 'database'
 
@@ -27,5 +30,20 @@ end
 mysql_database_user 'me' do
   connection connection_params
   password 'my_password_11'
-  action :create
+  privileges [:all]
+  action [:create, :grant]
 end
+
+# [2013-04-23T19:32:07+00:00] INFO: Processing chef_gem[mysql] action install (mysql::ruby line 36)
+# [2013-04-23T19:32:07+00:00] INFO: Processing mysql_database[my_db] action create (my_cookbook::default line 25)
+# [2013-04-23T19:32:07+00:00] INFO: Processing mysql_database_user[me] action create (my_cookbook::default line 30)
+
+
+#[2013-04-23T19:37:42+00:00] INFO: mysql_database_user[me]: granting access with statement [GRANT all ON *.* TO `me`@`localhost` IDENTIFIED BY [FILTERED]]
+
+#mysql> show databases;
+#+--------------------+
+#| Database           |
+#+--------------------+
+#| information_schema |
+#| my_db              |
