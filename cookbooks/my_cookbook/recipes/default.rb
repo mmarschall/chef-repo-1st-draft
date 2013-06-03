35708d451c45b204a8655bb69482835bd0a3c592
Matthias Marschall <mm@agileweboperations.com>
2013-06-03 22:33:39 +0200

    snmp installation

--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -6,40 +6,9 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+node.override['snmp']['syslocationVirtual'] = "Vagrant VirtualBox"
+node.override['snmp']['syslocationPhysical'] = "My laptop"
+node.override['snmp']['syscontact'] = "Root <root@localhost>"
+node.override['snmp']['full_systemview'] = true
+include_recipe "snmp"
 
-# needs in Rails 4.0 Gemfile
-# gem 'therubyracer', platforms: :ruby
-# gem 'unicorn'
-# enabled!!!
-
-include_recipe "apt"
-include_recipe "varnish::apt_repo"
-
-node.override['varnish']['storage_file'] = "/var/lib/varnish/vagrant/varnish_storage.bin"
-include_recipe "varnish"
-
-application "rails-app" do
-  packages %w[ruby1.9.3 runit git sqlite3 libsqlite3-dev]
-
-  path "/usr/local/www/rails-app"
-  owner "www-data"
-  group "www-data"
-
-  environment_name "development"
-
-  repository "https://github.com/mmarschall/rails-app.git"
-
-  rails do 
-    gems %w[bundler]
-
-    database_template "sqlite3_database.yml.erb"
-
-    database do
-      adapter "sqlite3"
-      database "db/rails-app.sqlite3"
-    end
-  end
-
-  unicorn do
-  end
-end
