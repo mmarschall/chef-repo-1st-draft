8743e18c718f395239e7a7bda51a3a34fcff7316
Matthias Marschall <mm@agileweboperations.com>
2013-06-19 22:38:32 +0200

    fail2ban

--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -12,5 +12,17 @@ iptables_rule "ssh"
 include_recipe "fail2ban"
 
 file "/etc/fail2ban/jail.local" do
-  
+  content <<-EOS
+  [ssh-ddos]
+
+enabled  = true
+port     = ssh
+filter   = sshd-ddos
+logpath  = /var/log/auth.log
+maxretry = 6
+  EOS
+    owner "root"
+    group "root"
+    mode 0644
+    notifies :restart, "service[fail2ban]"
 end
