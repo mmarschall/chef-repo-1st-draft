819a511f63bf7961abaca6c1368cb36c6c5f6b6e
Matthias Marschall <mm@agileweboperations.com>
2013-06-28 11:34:40 +0200

    Blueprint

--- cookbooks/my_cookbook/metadata.rb
+++ cookbooks/my_cookbook/metadata.rb
@@ -5,6 +5,6 @@ description      "Installs/Configures my_cookbook"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
-depends "chef-client"
 depends "apt"
-depends "ntp"
+depends "git"
+depends "python"
--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -7,5 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 include_recipe "apt"
-include_recipe "ntp"
-include_recipe "chef-client"
+include_recipe "git"
+include_recipe "python"
+
+python_pip "blueprint"
